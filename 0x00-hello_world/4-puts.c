b49785acc9c3d6cabba8487e48b82751760bf929
Malile <moeletsimalile@gmail.com>
2022-12-08 05:15:06 -0800

    hellow puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
-*main- Serves as the entry point for the program
+*main- entry point for the program
 *
-*Return: 0 always  
+*Return: 0 alwaysif succesful  
 */
 
 int main(void)
 
 {
-
-	        puts("\"Programming is like building a multilingual puzzle");
-
-	        return (0);
+	 puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
