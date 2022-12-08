a0394ead907710a1865fd7babbfe6a455ef07841
Malile <moeletsimalile@gmail.com>
2022-12-08 06:08:20 -0800

    👋, 📠F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
- * main -entry point
+ * main - entry point
  *
  * Return:0 if no errors, return non zero if errors
  */
 
-int main(void)
+itn main(void)
 {
+
 	printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 
