8c9ff3a4e8c3c02b34fe101b41ee61bda4261c5e
Malile <moeletsimalile@gmail.com>
2022-12-08 05:38:58 -0800

    👋,Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-*main- entry point for the program
-*
-*Return: 0 alwaysif succesful  
-*/
+ * main- Entry point
+ *
+ * Return:0 if no errors, return non zero if errors
+ */
 
 int main(void)
-
 {
-	 puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 
 }
