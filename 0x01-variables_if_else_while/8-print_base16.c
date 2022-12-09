f0fa8d9c0f611789165b1a92b760f97bcd083ccb
Malile <moeletsimalile@gmail.com>
2022-12-09 15:15:31 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char p;
+
+	int q;
+
+	p = 'a';
+	q = 0;
+	while
+		(q < 10) {
+			putchar(q + '0');
+			q++;
+		}
+	while
+		(p <= 'f') {
+			putchar(p);
+			p++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
