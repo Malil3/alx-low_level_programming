37e307514caaca2f4524096082d73f1ffaa03804
Malile <moeletsimalile@gmail.com>
2022-12-09 14:26:58 -0800

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int f;
+
+	f = 0;
+	while
+		(f < 10) {
+			putchar(f + '0');
+			f++;
+		}
+	putchar('\n');
+	return (0);
+}
+
+
+
