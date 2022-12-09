fc8e021e55a89c1ea66e16524c1e20da0ae7f04f
Malile <moeletsimalile@gmail.com>
2022-12-09 13:39:10 -0800

    📠🔤

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'z') {
+			if ((b != 'q' && b != 'e') && b <= 'z')
+				putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
