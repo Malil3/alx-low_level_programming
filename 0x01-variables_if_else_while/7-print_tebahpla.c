13c03bb5df8beaf29543ce589a32028f36290855
Malile <moeletsimalile@gmail.com>
2022-12-09 14:48:34 -0800

    7️⃣📠🔤

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char v;
+
+	v = 'z';
+	while
+		(v >= 'a') {
+			putchar(v);
+			v--;
+		}
+	putchar('\n');
+	return (0);
+}
