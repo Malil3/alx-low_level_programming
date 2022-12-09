4c93dd1e4b63bb9a68e3bf292d88d2310ca6e969
Malile <moeletsimalile@gmail.com>
2022-12-09 14:01:49 -0800

    5️⃣📠1️⃣2️⃣3️⃣4️⃣5️⃣6️⃣7️⃣8️⃣9️⃣1️⃣0️⃣

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int s;
+
+
+	for (s = 0; s <= 10; s++)
+		printf("%d", s);
+	putchar('\n');
+	return (0);
+}
