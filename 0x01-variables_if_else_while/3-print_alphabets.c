bfa7d2788accf18e7132549973b2f9a88c7ecc7e
Malile <moeletsimalile@gmail.com>
2022-12-09 12:59:09 -0800

    aldphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	char c;
+
+	char x;
+
+	c = 'a';
+	x = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(x <= 'Z') {
+			putchar(x);
+			x++;
+		}
+	return (0);
+
+}
+
+
