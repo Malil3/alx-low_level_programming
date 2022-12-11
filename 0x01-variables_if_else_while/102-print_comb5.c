4308c481833499fe1d7183e287bd6ea620aacbc9
Malile <moeletsimalile@gmail.com>
2022-12-11 09:42:59 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/*
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int l;
+	int m;
+
+	for (l = 0; l < 100; l++)
+	{
+		for (m = 0; m < 100; m++)
+		{
+			if (l < m)
+			{
+				putchar((l / 10) + 48);
+				putchar((l % 10) + 48);
+				putchar(' ');
+				putchar((m / 10) + 48);
+				putchar((m % 10) + 48);
+				if (l != 98 || m != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
