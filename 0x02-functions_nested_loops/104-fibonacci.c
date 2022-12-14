20522c4db9dc8530180de918796b555fab5c6f14
Malile <moeletsimalile@gmail.com>
2022-12-14 06:34:31 -0800

    nested C programming

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,8 @@ int main(void)
 	int i, boo, sawi;
 	long int c1, c2, candy, cake, c11, c22;
 
-	n1 = 1;
-	n2 = 2;
+	c1 = 1;
+	c2 = 2;
 	boo = sawi = 1;
 	printf("%ld, %ld", c1, c2);
 	for (i = 0; i < 96; i++)
