663d3d836b89a3b504e391a4895b19051b30a24c
Malile <moeletsimalile@gmail.com>
2022-12-14 02:04:41 -0800

    my nested c pramming tasks

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,14 +7,16 @@
  */
 void print_to_98(int n)
 {
-	while (n < 98)
+	while (n <= 98)
 	{
 		printf("%i, ", n);
 		n++;
 	}
-	while (n > 98)
+	while (n >= 98)
 	{
 		printf("%i, ", n);
 		n--;
 	}
+	printf("98");
+	_putchar('\n');
 }
