4f304b2e89441a2967ed8f0dfb699f42a1917a11
Malile <moeletsimalile@gmail.com>
2022-12-14 06:06:50 -0800

    nested C programming

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	long int c1, c2;
+	long int candy, sawi;
+
+	c1 = 1;
+	c2 = 2;
+	candy = sawi = 0;
+
+	while (candy <= 4000000)
+	{
+		candy = c1 + c2;
+		c1 = c2;
+		c2 = candy;
+		if ((c1 % 2) == 0)
+		{
+			sawi + = c1;
+		}
+	}
+	printf("%ld\n", sawi);
+	return (0);
+}
+
