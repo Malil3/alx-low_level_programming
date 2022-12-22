496d575235139f43559ee990e48b575939e924e8
Malile <moeletsimalile@gmail.com>
2022-12-22 07:38:08 -0800

    function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 char *leet(char *s)
 {
 	int string = 0, array;
-	int low_case[] = {97, 101, 111, 116, 108};
+	int lower_case[] = {97, 101, 111, 116, 108};
 	int upp_case[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
