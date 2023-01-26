ebb8617387e940202fdcfd8533fc2d59129b31d2
Malil3 <moeletsimalile@gmail.com>
2023-01-26 10:21:17 -0800

    Write a function that prints all the elements of a list_t list

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 /**
- * add_node - adds a new node at the beginning
+ * add_node_end - adds a new node at the en
  * of a list_t list.
  * @head: head of the linked list.
  * @str: string to store in the list.
  * Return: address of the head.
- */
-list_t *add_node(list_t **head, const char *str)
+ */ 
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new, *temp;
 	size_t nchar;
 
 	new = malloc(sizeof(list_t));
@@ -16,9 +16,20 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	new->str = strdup(str);
 	for (nchar = 0; str[nchar]; nchar++)
+
 		;
 	new->len = nchar;
-	new->next = *head;
-	*head = new;
+	new->next = NULL;
+	temp = *head;
+	if (temp == NULL)
+	{
+		*head = new;
+	}
+	else
+	{
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = new;
+	}
 	return (*head);
 }
